b536aa45ce6dc7c5a0484207f8be46290469b273
lisabronwyn <e.bronwyn@gmail.com>
2019-08-07 15:16:43 -0700

    changed app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express')
 const app = express()
 // set the port of our application
 // process.env.PORT lets the port be set by Heroku
-const port = 3000
+const port = process.env.PORT || 8080;
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
